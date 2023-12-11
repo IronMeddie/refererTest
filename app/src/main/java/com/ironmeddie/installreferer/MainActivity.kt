60bd4f4d22eb648e03f1236a3c311816be2be427
mikae <mikaelofarius@gmail.com>
2023-12-11 14:21:44 +0300

    3

--- app/src/main/java/com/ironmeddie/installreferer/MainActivity.kt
+++ app/src/main/java/com/ironmeddie/installreferer/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.ironmeddie.installreferer
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -16,7 +14,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -31,8 +28,9 @@ import com.ironmeddie.installreferer.ui.theme.InstallRefererTheme
 import io.appmetrica.analytics.AppMetrica
 import io.appmetrica.analytics.DeferredDeeplinkListener
 import io.appmetrica.analytics.DeferredDeeplinkParametersListener
+import kotlinx.coroutines.delay
 
-@OptIn(ExperimentalMaterial3Api::class)
+//@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
 
     private lateinit var referrerClient: InstallReferrerClient
@@ -71,34 +69,39 @@ class MainActivity : ComponentActivity() {
                 Box(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    val deeplink = rememberSaveable { mutableStateOf("") }
+//                    val deeplink = rememberSaveable { mutableStateOf("") }
                     val error = rememberSaveable { mutableStateOf("") }
                     val installReferer = rememberSaveable { mutableStateOf("") }
                     val appmetricaDataParams = rememberSaveable { mutableStateOf("") }
                     val appmetricaDeeplink = rememberSaveable { mutableStateOf("") }
+                    val isSuccess = rememberSaveable { mutableStateOf(false) }
 
                     LaunchedEffect(key1 = true) {
-                        try {
-                            val response: ReferrerDetails = referrerClient.installReferrer
-                            val referrerUrl: String = response.installReferrer
-                            val referrerClickTime: Long = response.referrerClickTimestampSeconds
-                            val appInstallTime: Long = response.installBeginTimestampSeconds
-                            val instantExperienceLaunched: Boolean = response.googlePlayInstantParam
-
-                            installReferer.value = referrerUrl + "\n\n\n" +
-                                    "referrerClickTime: " + referrerClickTime + "\n"
-                            "appInstallTime: " + appInstallTime + "\n"
-                            "instantExperienceLaunched: " + instantExperienceLaunched
-                            referrerClient.endConnection()
-                        } catch (e: Exception) {
-                            installReferer.value = e.message.toString()
+                        while (isSuccess.value == false) {
+                            try {
+                                installReferer.value = "connecting..."
+                                delay(100)
+                                val response: ReferrerDetails = referrerClient.installReferrer
+                                val referrerUrl: String = response.installReferrer
+                                val referrerClickTime: Long = response.referrerClickTimestampSeconds
+                                val appInstallTime: Long = response.installBeginTimestampSeconds
+                                val instantExperienceLaunched: Boolean =
+                                    response.googlePlayInstantParam
+                                installReferer.value =
+                                    "$referrerUrl\n\n\nreferrerClickTime: $referrerClickTime\nappInstallTime: $appInstallTime\ninstantExperienceLaunched: $instantExperienceLaunched"
+                                referrerClient.endConnection()
+                                isSuccess.value = true
+                            } catch (e: Exception) {
+                                installReferer.value = e.message.toString() + "\nretrying..."
+                                delay(1000)
+                            }
                         }
-
                     }
 
                     LazyColumn(
                         modifier = Modifier
-                            .fillMaxSize(),
+                            .fillMaxSize()
+                            .padding(horizontal = 16.dp),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
 
@@ -125,8 +128,6 @@ class MainActivity : ComponentActivity() {
                             Spacer(modifier = Modifier.height(24.dp))
                         }
 
-
-
                         item {
                             Text(
                                 text = appmetricaDataParams.value,
@@ -138,36 +139,7 @@ class MainActivity : ComponentActivity() {
                             Spacer(modifier = Modifier.height(24.dp))
                         }
 
-                        item {
-                            Text(
-                                text = appmetricaDeeplink.value,
-                                color = Color.Red,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(horizontal = 21.dp)
-                            )
-                            Spacer(modifier = Modifier.height(24.dp))
-                            TextField(value = deeplink.value, onValueChange = {
-                                deeplink.value = it
-                                error.value = ""
-                            })
-                            Spacer(modifier = Modifier.height(24.dp))
 
-                            Button(onClick = {
-                                try {
-                                    val uri = Uri.parse(deeplink.value)
-                                    val intent = Intent(Intent.ACTION_VIEW, uri)
-                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                                    startActivity(intent)
-                                    error.value = "success"
-                                } catch (t: Throwable) {
-                                    error.value = t?.localizedMessage ?: "unknown Error"
-                                }
-
-                            }, enabled = deeplink.value.isNotEmpty()) {
-                                Text(text = "open deeplink")
-                            }
-                        }
 
 
 
@@ -213,6 +185,37 @@ class MainActivity : ComponentActivity() {
                             Spacer(modifier = Modifier.height(24.dp))
                         }
 
+//                        item {
+//                            Text(
+//                                text = appmetricaDeeplink.value,
+//                                color = Color.Red,
+//                                modifier = Modifier
+//                                    .fillMaxWidth()
+//                                    .padding(horizontal = 21.dp)
+//                            )
+//                            Spacer(modifier = Modifier.height(24.dp))
+//                            TextField(value = deeplink.value, onValueChange = {
+//                                deeplink.value = it
+//                                error.value = ""
+//                            })
+//                            Spacer(modifier = Modifier.height(24.dp))
+//
+//                            Button(onClick = {
+//                                try {
+//                                    val uri = Uri.parse(deeplink.value)
+//                                    val intent = Intent(Intent.ACTION_VIEW, uri)
+//                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//                                    startActivity(intent)
+//                                    error.value = "success"
+//                                } catch (t: Throwable) {
+//                                    error.value = t?.localizedMessage ?: "unknown Error"
+//                                }
+//
+//                            }, enabled = deeplink.value.isNotEmpty()) {
+//                                Text(text = "open deeplink")
+//                            }
+//                        }
+
 
                     }
 
@@ -220,6 +223,8 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
 }
 
 
